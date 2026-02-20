412f6de6082c5fc9193142ce13740dfe377ae465
horizon-spark <166980733+horizon-spark@users.noreply.github.com>
2026-02-20 12:09:03 +0300

    Create app layout and basic components

---
+++ AndroidGeoDataApp/app/+not-found.tsx
@@ -0,0 +1,32 @@
+import { Link, Stack } from "expo-router";
+import { StyleSheet, View } from "react-native";
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Not found" }} />
+      <View style={styles.container}>
+        <Link href="/" style={styles.button}>
+          Back to Home screen
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
